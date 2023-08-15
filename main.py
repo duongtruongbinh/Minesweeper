6e3850d0457b6d4d74deb93b17a822588768dcab
duongtruongbinh <duongtruongbinh2003@gmail.com>
2023-08-15 08:29:51 +0700

    Demo Completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# from utility import output, get_problem
-# from Solver.pysat import solve_minesweeper_pysat
+import utility
+from Solver.pysat import solve_minesweeper_pysat
 from Solver.astar import solve_minesweeper_astar
 from Solver.bruteforce import solve_minesweeper_bruteforce
 from Solver.backtracking import solve_minesweeper_backtracking
-from Solver.pysat import PYSATSolver
+
 
 if __name__ == "__main__":
 
@@ -13,20 +13,25 @@ if __name__ == "__main__":
     print("3. SAT")
     print("4. A*")
 
-    algorithm = int(input("Algorithm: "))
-    state = [[2, 0, 0, 1], [2, 0, 3, 1], [1, 1, 1, 0]]
-    print(state)
+    algorithm = int(input("Enter the number of the algorithm: "))
+    state = utility.read_state("input.txt")
+    print("Input: ")
+
+    for row in state:
+        print(",".join(map(str, row)))
+    print()
 
     if algorithm == 1:
         solution = solve_minesweeper_bruteforce(state)
     elif algorithm == 2:
         solution = solve_minesweeper_backtracking(state)
     elif algorithm == 3:
-        solution = PYSATSolver(state)
+        solution = solve_minesweeper_pysat(state)
     elif algorithm == 4:
         solution = solve_minesweeper_astar(state)
 
     if solution:
+        print("Solution found")
         for assignment in solution:
             if assignment > 0:
                 i = (assignment - 1) // len(state[0])
@@ -35,8 +40,7 @@ if __name__ == "__main__":
 
         for row in state:
             print(",".join(map(lambda x: "*" if x == -1 else str(x), row)))
-            # print("\n")
+        print()
 
-        print("Done")
     else:
         print('No solution')
